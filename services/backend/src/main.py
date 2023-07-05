48c11041351a3c10f5401494122d3fec1e3f512f
Lam Pham <phamlamuet@gmail.com>
2023-07-05 15:37:26 +0700

    update ignore and venv

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,11 +1,21 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 
 # uvicorn main:app --reload
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 def home():
-    return {"Hello": "World"}
+    return {"Hello": "World123"}
